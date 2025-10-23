05e039cc6dc13e5705a18a65fdb1bd5db204e5c5
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-10-23 19:42:30 +0600

    responsive bottles

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,9 +1,12 @@
+import './Bottle.css';
+
 const Bottle = ({bottle}) => {
     const {name, img, price}=bottle;
     return (
-        <div>
+        <div className="bottle">
             <h3>Bottle: {name}</h3>
             <img src={img} alt="" />
+            <p>Price: ${price}</p>
         </div>
     );
 };
